4b6b19b63eeafdd28ab637e828cbd8e1d14cdc13
Caleb Atemnkeng <troisj3@yahoo.com>
2022-08-22 12:48:50 -0400

    better

--- b1.py
+++ b1.py
@@ -1,3 +1,5 @@
 #nothing good comes easy
 #Next time run git commit -a -m to commit directly without having to add first to stagin area
 #Someday we will all get better
+echo "the more we are together"
+echo "the happier we will be"
